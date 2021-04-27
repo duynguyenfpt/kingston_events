7ff2e0d4eabcf9d4c2d86a445fdf11427c90978d
Long Vu <vulong22121999@gmail.com>
2021-04-27 13:11:25 +0700

    Fix responsive

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-    if($( window ).width() < 900){
-        $(".fs-gift-detail").css("font-size", "5px");
-    }
- });
+// $(document).ready(function(){
+//     if($( window ).width() < 900){
+//         $(".fs-gift-detail").css("font-size", "5px");
+//     }
+//  });
